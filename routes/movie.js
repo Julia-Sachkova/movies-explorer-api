f17d4d389fa9f66920c0359a484587e18bc6d5a2
Julia-Rulova <julia.rulova0066@gmail.com>
2022-08-15 18:49:04 +0300

    fix/movieId 2

--- routes/movie.js
+++ routes/movie.js
@@ -26,7 +26,7 @@ router.post('/movies', celebrate({
 }), createMovie);
 router.delete('/movies/:movieId', celebrate({
   params: Joi.object().keys({
-    movieId: Joi.number().required(),
+    movieId: Joi.string().required(),
   }),
 }), deleteMovie);
 
